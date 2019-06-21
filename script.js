5081cf16b9e3df8680ac786249779509b8284866
ruzhickii <ruzhickiisasha@gmail.com>
2019-06-21 18:28:23 +0300

    fixed bugs & added poster image of movie & some styles

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
+const urlPoster = 'https://image.tmdb.org/t/p/w500';
 
 function apiSearch(event) {
     event.preventDefault();
@@ -9,20 +10,29 @@ function apiSearch(event) {
 
     fetch(server)
         .then(function(value) {
+            if(value.status !== 200) {
+                return Promise.reject(value);
+            }
             return value.json();
         })
         .then(function(output) {
+            console.log('!!!', output);
             let inner = '';
             output.results.forEach(function(item) {
                 console.log( item );
                 let nameItem = item.name || item.title;
-                inner +=`<div class = "col-12 col-md-4 col-xl-3"> ${nameItem}</div>`;
+                inner += `
+                    <div class = "col-12 col-md-4 col-xl-3 item"> 
+                        <img src = '${urlPoster + item.poster_path}' alt = '${nameItem}'>
+                        <h5> ${nameItem} </h5>
+                    </div>
+            `;
             });
             movie.innerHTML = inner;
         })
         .catch(function(reason) {
             movie.innerHTML = 'Что то пошло совсем не так !';
-            console.log('error: ' + reason.status);
+            console.error('error: ' + reason.status);
         });
 
 }
